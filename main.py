43cad52b5129b138e2797475a7e3c3dd3de8400f
frozendolphin <122843077+frozendolphin@users.noreply.github.com>
2025-03-04 10:24:27 -0800

    added circleshape class and single playerobject

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     time_clock = pygame.time.Clock()
     dt = 0
-    
+    single_player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill("black")
+        single_player.draw(screen)
         pygame.display.flip()
         dt = time_clock.tick(60) / 1000
     
